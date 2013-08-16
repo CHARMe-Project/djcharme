49dcfb7705ca4d638b533d8ff7ef6ea79e69a6db
kusamau <m.nagni@gmail.com>
2013-08-16 12:15:51 +0100

    Update the Access-Control-Allow-Headers

--- djcharme/djcharme/charme_middleware.py
+++ djcharme/djcharme/charme_middleware.py
@@ -103,7 +103,7 @@ class CharmeMiddleware(object):
     def process_response(self, request, response):
         if request.method == 'OPTIONS':
             response['Access-Control-Allow-Methods'] = "POST, GET, OPTIONS"                 
-            response['Access-Control-Allow-Headers'] = "origin, x-requested-with, content-type"
+            response['Access-Control-Allow-Headers'] = "X-Requested-With"
             response['Access-Control-Allow-Origin'] = request.META.get('HTTP_ORIGIN', 'http://localhost:8000')
             response['Access-Control-Max-Age'] = 10
             response['Content-Type'] = "text/plain"
