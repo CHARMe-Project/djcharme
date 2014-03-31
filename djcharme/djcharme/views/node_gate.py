40daa2570f089ab23be2ab16d9d68cbefaa6ad74
Antony Wilson <antony.wilson@stfc.ac.uk>
2014-03-31 11:12:20 +0100

    remove graph from return of insert

--- djcharme/djcharme/views/node_gate.py
+++ djcharme/djcharme/views/node_gate.py
@@ -10,7 +10,7 @@ from djcharme.node.actions import (OA, FORMAT_MAP, find_annotation_graph,
 from djcharme import mm_render_to_response, mm_render_to_response_error
 from djcharme.exception import StoreConnectionError
 from djcharme.views import (isPOST, content_type, validateMimeFormat,
-                            isOPTIONS, http_accept, get_format, checkMimeFormat, 
+                            isOPTIONS, http_accept, get_format, checkMimeFormat,
                             get_depth)
 
 from django.http.response import HttpResponseRedirectBase, Http404, HttpResponse
@@ -36,11 +36,11 @@ def __serialize(graph, req_format='application/rdf+xml'):
         Serializes a graph according to the required format
         - rdflib:Graph **graph** the graph to serialize
         - string **req_format** the serialization format
-        - **return** the serialized graph                
-    '''         
+        - **return** the serialized graph
+    '''
     if req_format == FORMAT_MAP['json-ld']:
         req_format = 'json-ld'
-        
+
     return graph.serialize(format=req_format)
 
 
@@ -56,13 +56,13 @@ def index(request, graph='stable'):
     tmp_g = None
     try:
         tmp_g = _collect_annotations(graph)
-    except StoreConnectionError as e:
-        messages.add_message(request, messages.ERROR, e)
+    except StoreConnectionError as ex:
+        messages.add_message(request, messages.ERROR, ex)
         return mm_render_to_response_error(request, '503.html', 503)
-        
-                
+
+
     req_format = validateMimeFormat(request)
-    
+
     if req_format is not None:
         LOGGING.debug("Annotations %s" % 
                       __serialize(tmp_g, req_format=req_format))
@@ -70,12 +70,12 @@ def index(request, graph='stable'):
     elif 'text/html' in http_accept(request):
         states = {}
         LOGGING.debug("Annotations %s" % tmp_g.serialize())
-        for s, p, o in tmp_g.triples((None, None, OA['Annotation'])):
-            states[s] = find_annotation_graph(s)   
-            
+        for subject, pred, obj in tmp_g.triples((None, None, OA['Annotation'])):
+            states[subject] = find_annotation_graph(subject)
+
         context = {'results': tmp_g.serialize(), 'states': json.dumps(states)}
         return mm_render_to_response(request, context, 'viewer.html')
-    
+
     messages.add_message(request, messages.ERROR, "Format not accepted")
     return mm_render_to_response_error(request, '400.html', 400)
 
@@ -83,33 +83,33 @@ def index(request, graph='stable'):
 def __get_ret_format(request, req_format):
     '''
         Extracts the return format otherwise return the req_format
-    '''    
+    '''
     ret_format = http_accept(request)
     if type(ret_format) == list:
         ret_format = ret_format[0]
 
     if ret_format is None:
         ret_format = req_format
-    else:            
+    else:
         ret_format = checkMimeFormat(ret_format)
 
     if ret_format is None:
-        ret_format = req_format    
+        ret_format = req_format
     return ret_format
 
 
 def __get_req_format(request):
     '''
         Extracts the request format otherwise return the req_format
-    '''     
+    '''
     return checkMimeFormat(content_type(request))
 
 # Temporary solution as long identify a solution for csrf
-#@csrf_protect
-@csrf_exempt  
+# @csrf_protect
+@csrf_exempt
 def insert(request):
     '''
-        Inserts in the triplestore a new annotation under the "ANNO_SUBMITTED" 
+        Inserts in the triplestore a new annotation under the "ANNO_SUBMITTED"
         graph
     '''
     '''
@@ -121,67 +121,68 @@ def insert(request):
     '''
     req_format = __get_req_format(request)
     ret_format = __get_ret_format(request, req_format)
-    
-    if req_format is None:        
-        messages.add_message(request, messages.ERROR, 
+
+    if req_format is None:
+        messages.add_message(request, messages.ERROR,
                              "Cannot ingest the posted format")
         return mm_render_to_response_error(request, '400.html', 400)
-    
+
     if isPOST(request) or isOPTIONS(request):
         triples = request.body
-        tmp_g = insert_rdf(triples, req_format, graph=ANNO_SUBMITTED)        
-        return HttpResponse(__serialize(tmp_g, req_format = ret_format), 
-                            content_type=FORMAT_MAP.get(ret_format))
-
+        insert_rdf(triples, req_format, graph=ANNO_SUBMITTED)        
+        return HttpResponse(None, content_type=FORMAT_MAP.get(ret_format))
 
 # Temporary solution as long identify a solution for csrf
-#@csrf_protect
-@csrf_exempt         
+# @csrf_protect
+@csrf_exempt
 def advance_status(request):
     '''
         Advance the status of an annotation
-    '''            
+    '''
     if isPOST(request) and 'application/json' in content_type(request):
-        params = json.loads(request.body) 
+        params = json.loads(request.body)
         if not params.has_key('annotation') or not params.has_key('state'):
-            messages.add_message(request, messages.ERROR, "Missing annotation/state parameters")
+            messages.add_message(request, messages.ERROR,
+                                 "Missing annotation/state parameters")
             return mm_render_to_response_error(request, '400.html', 400)
-        LOGGING.info("advancing %s to state:%s" % (params.get('annotation'), params.get('toState')))
-        tmp_g = change_annotation_state(params.get('annotation'), params.get('toState'))
-        
+        LOGGING.info("advancing %s to state:%s" % (params.get('annotation'),
+                                                   params.get('toState')))
+        tmp_g = change_annotation_state(params.get('annotation'),
+                                        params.get('toState'))
+
         return HttpResponse(tmp_g.serialize())
-        
-        
-def process_resource(request, resource_id):   
+
+
+def process_resource(request, resource_id):
     if validateMimeFormat(request) is not None:
         getformat = get_format(request)
         path = "/%s/%s" % (DATA, resource_id)
         if getformat is not None:
-            path = "%s/?format=%s" % (path, getformat)                           
+            path = "%s/?format=%s" % (path, getformat)
         LOGGING.info("Redirecting to %s" % path)
         return HttpResponseSeeOther(path)
-    
+
     if 'text/html' in http_accept(request):
         LOGGING.info("Redirecting to /%s/%s" % (PAGE, resource_id))
         return HttpResponseSeeOther('/%s/%s' % (PAGE, resource_id))
     return Http404()
-        
+
 def process_data(request, resource_id):
     if get_format(request) is None and 'text/html' in http_accept(request):
-        return process_resource(request, resource_id = resource_id)
-    
+        return process_resource(request, resource_id=resource_id)
+
     req_format = validateMimeFormat(request)
     if req_format is None:
         return process_resource(request, resource_id)
-            
-    tmp_g = find_resource_by_id(resource_id, get_depth(request))           
-    return HttpResponse(tmp_g.serialize(format = req_format), 
-                            mimetype = FORMAT_MAP.get(req_format))  
 
-def process_page(request, resource_id = None):
+    tmp_g = find_resource_by_id(resource_id, get_depth(request))
+    return HttpResponse(tmp_g.serialize(format=req_format),
+                            mimetype=FORMAT_MAP.get(req_format))
+
+def process_page(request, resource_id=None):
     if 'text/html' not in http_accept(request):
         return process_resource(request, resource_id)
-        
-    tmp_g = find_resource_by_id(resource_id, get_depth(request))                 
+
+    tmp_g = find_resource_by_id(resource_id, get_depth(request))
     context = {'results': tmp_g.serialize()}
     return mm_render_to_response(request, context, 'viewer.html')
