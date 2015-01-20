cadc2eecd5afe7bce6ca59c6259308a7b81ed15b
antony-wilson <antony.wilson@stfc.ac.uk>
2015-01-20 11:18:51 +0000

    display more info

--- djcharme/djcharme/static/charme/rdf/Triple.js
+++ djcharme/djcharme/static/charme/rdf/Triple.js
@@ -4,7 +4,7 @@ define(["dojox/xml/parser", "dojox/grid/DataGrid", "dojo/store/Memory",
 		function(parser, DataGrid, Memory, ObjectStore, gridCells, lang, baseArray, JSON){
 			return {
 				describeAnnotation: function(xml, states, div_id){
-					var store = new Memory({ idProperty: "annotation" });
+					var store = new Memory({ idProperty: "key" });
 
 					var xmldom = parser.parse(xml);
 					var nsResolver = document.createNSResolver( xmldom.ownerDocument == null ? 
@@ -22,26 +22,67 @@ define(["dojox/xml/parser", "dojox/grid/DataGrid", "dojo/store/Memory",
 
 					var value = queryResult.iterateNext();
 					var storedAnnotation = '';
-		            while(value){
-		            	store.put({annotation:value.getAttribute('rdf:about')});
-		            	storedAnnotation = store.get(value.getAttribute('rdf:about'));
-		            	storedAnnotation.state = "None"
-		            	if (states_obj != null) {
-		            		storedAnnotation.state = states_obj[storedAnnotation.annotation];	
-		            	}		            	
+					var keyValue = 0;
+		            while(value){            	
 		            	var nodes = value.childNodes;
 		            	var child = "";
 		            	for(i=0; i<nodes.length; i++) {
 		            		child = nodes[i];
 		            		if (child.localName == "hasBody") {
-		            			storedAnnotation.hasBody = child.getAttribute('rdf:resource')		            			
-		            		}
+		            			storedAnnotation=getStoredAnnotation(store, value)
+		            			storedAnnotation.property = "hasBody"		            			
+			            		storedAnnotation.value = child.getAttribute('rdf:resource');
+				            	continue
+		            		} 
 		            		if (child.localName == "hasTarget") {
-		            			storedAnnotation.hasTarget = child.getAttribute('rdf:resource');
-		            		}
-		            		if (storedAnnotation.hasBody && storedAnnotation.hasTarget) { 
-		            			break;
-		            		}
+		            			storedAnnotation=getStoredAnnotation(store, value)
+		            			storedAnnotation.property = "hasTarget"		            			
+		            			storedAnnotation.value = child.getAttribute('rdf:resource');
+				            	continue
+				            }
+		            		if (child.localName == "annotatedBy") {
+		            			storedAnnotation=getStoredAnnotation(store, value)
+		            			storedAnnotation.property = "annotatedBy"		            			
+			            		storedAnnotation.value = child.getAttribute('rdf:resource');
+				            	continue
+				            }
+		            		if (child.localName == "annotatedAt") {
+		            			storedAnnotation=getStoredAnnotation(store, value)
+		            			storedAnnotation.property = "annotatedAt"		            			
+				            	storedAnnotation.value = child.textContent;
+				            	continue
+				            }
+		            		if (child.localName == "accountName") {
+		            			storedAnnotation=getStoredAnnotation(store, value)
+		            			storedAnnotation.property = "accountName"		            			
+				            	storedAnnotation.value = child.textContent;
+				            	continue
+				            }
+		            		if (child.localName == "givenName") {
+		            			storedAnnotation=getStoredAnnotation(store, value)
+		            			storedAnnotation.property = "givenName"		            			
+				            	storedAnnotation.value = child.textContent;
+				            	continue
+				            }
+		            		if (child.localName == "familyName") {
+		            			storedAnnotation=getStoredAnnotation(store, value)
+		            			storedAnnotation.property = "familyName"		            			
+				            	storedAnnotation.value = child.textContent;
+				            	continue
+				            }
+		            		if (child.localName == "name") {
+		            			storedAnnotation=getStoredAnnotation(store, value)
+		            			storedAnnotation.property = "name"		            			
+				            	storedAnnotation.value = child.textContent;
+				            	keyValue++
+				            	continue
+				            }		            			
+		            		if (child.localName == "type") {
+		            			storedAnnotation=getStoredAnnotation(store, value)
+		            			storedAnnotation.property = "type"		            			
+				            	storedAnnotation.value = child.getAttribute('rdf:resource');
+				            	continue
+				            }		            		
 		            	}
 		                value = queryResult.iterateNext();
 		            }
@@ -55,22 +96,21 @@ define(["dojox/xml/parser", "dojox/grid/DataGrid", "dojo/store/Memory",
 						structure: 
 						            [
 						             new gridCells.RowIndex({ width: "10%" }),
-						             {name: "Annotation", field: "annotation", width: "100%",
+						             {name: "Resource", field: "annotation", width: "100%",
 						            	 formatter: function(value) {  
 						            	 	return "<a href=\'" + value + "\'>" + value + "</a>"; 
 						             	 }
 						             },
-						             {name: "hasTarget", field: "hasTarget", width: "100%",
+						             {name: "Property", field: "property", width: "100%",
 						            	 formatter: function(value) {  
-					            	 		return "<a href=\'" + value + "\'>" + value + "</a>"; 
+					            	 		return value; 
 					             	 	}
 						             },
-						             {name: "hasBody", field: "hasBody", width: "100%",
+						             {name: "Value", field: "value", width: "100%",
 					            	 formatter: function(value) {  
 				            	 		return "<a href=\'" + value + "\'>" + value + "</a>"; 				             	 	
 						             	}
 						             },						             
-						             {name: "state", field: "state", width: "100%"}
 						            ],
 						selectionMode: "multiple"
 					 },div_id); 
@@ -83,6 +123,15 @@ define(["dojox/xml/parser", "dojox/grid/DataGrid", "dojo/store/Memory",
 						var msg = "You have selected row" + ((tmp.length > 1) ? "s ": " ");
 						node.innerHTML = msg + tmp.join(", ");
 					}
+					
+					function getStoredAnnotation(store,value){
+		            	keyValue++
+		            	store.put({key:keyValue});
+		            	storedAnnotation = store.get(keyValue);
+		            	storedAnnotation.annotation=value.getAttribute('rdf:about')
+		            	return storedAnnotation
+					}
+					
 					grid.on("SelectionChanged",
 					lang.hitch(this, reportSelection, document.getElementById("clicked")), true);
 				
